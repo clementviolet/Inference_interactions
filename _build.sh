df7599fca8a1c9788945f5677bf051b5c043aedc
Clement VIOLET <22980442+clementviolet@users.noreply.github.com>
2020-01-17 16:10:50 +0100

    New setup, trying packages with the same dataset

--- _build.sh
+++ _build.sh
@@ -3,3 +3,5 @@
 set -ev
 
 Rscript -e "rmarkdown::render('index.Rmd', output_format = rmdformats::readthedown(), output_file = 'index.html', output_dir = '_site')"
+Rscript -e "rmarkdown::render('test_packages.Rmd', output_format = rmdformats::readthedown(), output_file = 'test_packages.html', output_dir = '_site')"
+Rscript -e "rmarkdown::render('inference_interractions.Rmd', output_format = rmdformats::readthedown(), output_file = 'inference_interractions.html', output_dir = '_site')"
