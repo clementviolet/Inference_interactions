0b602100a6093c127410c1d3fff9221b4e2160cc
Clement VIOLET <22980442+clementviolet@users.noreply.github.com>
2020-01-24 13:53:20 +0100

    Do not render useless files

--- _build.sh
+++ _build.sh
@@ -3,6 +3,6 @@
 set -ev
 
 Rscript -e "rmarkdown::render('index.Rmd', output_format = rmdformats::readthedown(), output_file = 'index.html', output_dir = '_site')"
-Rscript -e "rmarkdown::render('test_packages.Rmd', output_format = rmdformats::readthedown(), output_file = 'test_packages.html', output_dir = '_site')"
-Rscript -e "rmarkdown::render('inference_interractions.Rmd', output_format = rmdformats::readthedown(), output_file = 'inference_interractions.html', output_dir = '_site')"
-Rscript -e "rmarkdown::render('gglvm.Rmd', output_format = rmdformats::readthedown(), output_file = 'inference_interractions.html', output_dir = '_site')
+# Rscript -e "rmarkdown::render('test_packages.Rmd', output_format = rmdformats::readthedown(), output_file = 'test_packages.html', output_dir = '_site')"
+# Rscript -e "rmarkdown::render('inference_interractions.Rmd', output_format = rmdformats::readthedown(), output_file = 'inference_interractions.html', output_dir = '_site')"
+Rscript -e "rmarkdown::render('gllvm.Rmd', output_format = rmdformats::readthedown(), output_file = 'gllvm.html', output_dir = '_site')"
